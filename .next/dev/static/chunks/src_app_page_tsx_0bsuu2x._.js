6f991e04cdfd7fdeec56ae6cc863e3fa7f4d805c
Reiji74 <danishimansufian74@gmail.com>
2026-04-30 15:16:30 +0800

    chore: remove .next folder from git tracking

--- .next/dev/static/chunks/src_app_page_tsx_0bsuu2x._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_057v46v._.js","static/chunks/src_0xu~i7-._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_projects_[id]_page_tsx_0bsuu2x._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0ib-b7y._.js","static/chunks/node_modules_next_06-9rlh._.js","static/chunks/node_modules_refractor_lang_0zgbiwj._.js","static/chunks/node_modules_refractor_lib_0u2cgry._.js","static/chunks/node_modules_0w_.qv5._.js"],
-    source: "dynamic"
-});
